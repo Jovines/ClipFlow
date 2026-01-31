76d675fdde52c59c8849c452676f2e3040949f42
Jovines <jovines@qq.com>
2026-01-31 20:12:42 +0800

    fix: hide group panel when mouse leaves compact item rows

--- Sources/Views/Components/GroupView.swift
+++ Sources/Views/Components/GroupView.swift
@@ -113,6 +113,9 @@ struct CompactItemRow: View {
         .contentShape(Rectangle())
         .onHover { hovering in
             isHovered = hovering
+            if !hovering {
+                panelCoordinator.hidePanel()
+            }
         }
         .onTapGesture(perform: onSelect)
     }
